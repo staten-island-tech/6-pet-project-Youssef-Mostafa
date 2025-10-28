3f1cdfcf3818e29cec9e359f0afd0d64ed046d95
Youssef Mostafa <youssefm32@nycstudents.net>
2025-10-28 10:15:54 -0400

    Update pet.py

--- pet.py
+++ pet.py
@@ -1,6 +1,6 @@
-# Activity 1
+#Examples
 
-class Hero:
+""" class Hero:
     def __init__(self, name, money, inventory):
         self.name = name
         self.money = money
@@ -8,10 +8,26 @@ class Hero:
 
     def buy(self, item):
         self.inventory.append(item)
-        print(self.inventory)
 Khaby_Lame_Mechanism = Hero("Khaby_Lame_Mechanism", 500, ["Hands of Fate"])
 
 print(Khaby_Lame_Mechanism.__dict__)
 
-Khaby_Lame_Mechanism.buy({"item": "Upraded Hands of Time", "attack": 61})
+Khaby_Lame_Mechanism.buy({"item": "Upraded Hands of Time", "attack": 61})   
+print(Khaby_Lame_Mechanism.__dict__)
+ """
+#Personal Pet Project
+
+import random
+
+
+class Pet:
+    def __init__ (self, name, happiness):
+        self.name = name
+        self.happiness = happiness
+
+    def play(self, happiness):
+        self.happiness += random.randint(1,6)
+        print (f"Khaby Lame Mechanism turns his hands with excitement. Happiness increased by {self.happiness}.")
+Khaby_Lame_Mechanism = Pet("Khaby_Lame_Mechanism", 0)
+Khaby_Lame_Mechanism.play(1)
 print(Khaby_Lame_Mechanism.__dict__)
